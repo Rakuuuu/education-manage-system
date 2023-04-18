e41770b78d3a4a3c1ba99731ada2a7a36d8b01b3
Rakuuuu <1506421526@qq.com>
2023-04-18 16:15:42 +0800

    加入签到功能

--- src/views/sign-in/signIn.vue
+++ src/views/sign-in/signIn.vue
@@ -2,9 +2,11 @@
   <div class="container">
     <div class="container-main">
       <el-card>
-        <h3>输入签到码以进行签到</h3>
-        <el-input v-model="signInCode"/>
-        <el-button type="primary" @click="submitSignIn">签到</el-button>
+        <h3>在线签到</h3>
+        <div>
+          <el-input v-model="signInCode" placeholder="请输入签到码"/>
+          <el-button @click="submitSignIn">签到</el-button>
+        </div>
       </el-card>
     </div>
   </div>
@@ -23,10 +25,10 @@ export default {
   methods: {
     submitSignIn () {
       userApi.doRecord({
-        token: this.signInCode,
-        id: 1
+        token: parseInt(this.signInCode),
+        studentId: 1
       }).then(res => {
-        console.log(res)
+        if (res.code === 1) { this.$message.success(res.message) } else { this.$message.warning(res.message) }
       }, err => {
         console.log(err)
       })
@@ -68,10 +70,15 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    background: url(~@/assets/bg.gif) top center no-repeat;
+    background-size: 100% 100%;
+    overflow: hidden;
+    border: 0 solid;
+    box-shadow: 0 10px 20px rgba(0,0,0,.3);
   }
 
   >>>.el-card>div{
-    height: 250px;
+    height: 300px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -80,11 +87,15 @@ export default {
 
   >>>.el-card h3{
     text-align: center;
+    letter-spacing: 0.5rem;
+    margin:30px -0.5rem 0 0 ;
+    font-size: 48px;
+    color: #444444;
   }
 
   >>>.el-card .el-button{
     width: 100%;
-    margin-top: 20px;
+    margin-top: 40px;
     justify-self: start;
   }
 
